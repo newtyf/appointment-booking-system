86efd88966ff36d56f025985ee639fe56eb19752
newtyf <axelskam.123@outlook.es>
2025-07-06 11:58:10 -0500

    feat(backend): :sparkles: Se desarrollo el servicio para la gestion de servicios de la empresa

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -2,5 +2,6 @@ from app.services.user_service import UserService
 from app.services.auth_service import AuthService
 from app.services.role_service import RoleChecker
 from app.services.appointment_service import AppointmentService
+from app.services.service_service import ServiceService
 
-__all__ = ["UserService", "AuthService", "RoleChecker", "AppointmentService"]
+__all__ = ["UserService", "AuthService", "RoleChecker", "AppointmentService", "ServiceService"]
