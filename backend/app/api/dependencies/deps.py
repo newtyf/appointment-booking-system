4d152b111b68173da49f58ce6cbdc7683b942737
newtyf <axelskam.123@outlook.es>
2025-07-06 11:59:54 -0500

    feat(backend): :sparkles: Se agregaron los endpoints para el crud de servicios, ademas se actualizo el schema de autenticacion para usar el auth swagger ui de manera mas intuitica

--- backend/app/api/dependencies/deps.py
+++ backend/app/api/dependencies/deps.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_db_session
 from app.models.user import User
-from app.services import AuthService, RoleChecker, UserService, AppointmentService
+from app.services import AuthService, RoleChecker, UserService, AppointmentService, ServiceService
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+http_bearer_scheme = HTTPBearer()
 
 def get_user_service(db: Annotated[AsyncSession, Depends(get_db_session)]) -> UserService:
     return UserService(db)
@@ -22,12 +22,15 @@ def get_auth_service(
 def get_appointment_service(db: AsyncSession = Depends(get_db_session)) -> AppointmentService:
     return AppointmentService(db)
 
+def get_service_service(db: AsyncSession = Depends(get_db_session)) -> ServiceService:
+    return ServiceService(db)
+
 # Dependencia para validar si el usuario está autenticado y obtener su información
 async def get_current_user(
-    token: Annotated[str, Depends(oauth2_scheme)],
+    token: Annotated[HTTPAuthorizationCredentials, Depends(http_bearer_scheme)],
     auth_service: Annotated[AuthService, Depends(get_auth_service)]
 ) -> User:
-    user = await auth_service.get_user_from_token(token)
+    user = await auth_service.get_user_from_token(token.credentials)
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
